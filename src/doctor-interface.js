3af72c56d32672635fe13c12b67814829354ce8d
Austin Barr <austinbarr@protonmail.com>
2018-08-10 12:09:06 -0700

    added adress and phone number to serach results

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -16,9 +16,19 @@ $(function () {
     promise.then(function (response) {
       let doctor = JSON.parse(response);
       for(let i = 0; i < doctor.data.length; i++){
-        $("#output").append('<li>' + doctor.data[i].profile.first_name + '<li>');
+        $("#output").append("<div>" +        
+         '<h2>' + doctor.data[i].profile.first_name + " " + doctor.data[i].profile.last_name + '</h2>' +
+         '<p>' + doctor.data[i].practices[0].phones[0].number + '</p>' +
+         '<p>' + doctor.data[i].practices[0].visit_address.street + '</p>' +
+         '<p>' + doctor.data[i].practices[0].visit_address.city + "," +
+         doctor.data[i].practices[0].visit_address.state +  " " +
+         doctor.data[i].practices[0].visit_address.zip +  
+         '</p>' +
+         "</div>" 
+        );
       }
     }, function (error) {
+      $("#error").text(`there was an Error:${error.message}`);
       $("#error").text(`there was an Error:${error.message}`);
     });
   });
