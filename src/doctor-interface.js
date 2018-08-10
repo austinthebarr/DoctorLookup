278447c3c9bfd626dcca6d8d33f7375c4808ddc5
Austin Barr <austinbarr@protonmail.com>
2018-08-10 13:26:11 -0700

    added a fail statement, if no responses will tell user

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -7,29 +7,39 @@ import {
 
 $(function () {
   $("#bykeyWord").click(function () {
+    $("#output").empty();
     const input = $("#userInput").val();
     $("#userInput").val("");
 
     let newDoctor = new Doctor();
-    let promise = newDoctor.findDoctorByKey(input);
+    let keyPromise = newDoctor.findDoctorByKey(input);
 
-    promise.then(function (response) {
+    keyPromise.then(function (response) {
       let doctor = JSON.parse(response);
-      for(let i = 0; i < doctor.data.length; i++){
-        $("#output").append("<div>" +        
-         '<h2>' + doctor.data[i].profile.first_name + " " + doctor.data[i].profile.last_name + '</h2>' +
-         '<p>' + doctor.data[i].practices[0].phones[0].number + '</p>' +
-         '<p>' + doctor.data[i].practices[0].visit_address.street + '</p>' +
-         '<p>' + doctor.data[i].practices[0].visit_address.city + "," +
-         doctor.data[i].practices[0].visit_address.state +  " " +
-         doctor.data[i].practices[0].visit_address.zip +  
-         '</p>' +
-         "</div>" 
-        );
+      if (doctor == 0) {
+        for (let i = 0; i < doctor.data.length; i++) {
+          $("#output").append("<div>" +
+            '<h2>' + doctor.data[i].profile.first_name + " " + doctor.data[i].profile.last_name + '</h2>' +
+            '<p>' + "Phone Number: " + doctor.data[i].practices[0].phones[0].number + '</p>' +
+            '<p>' + doctor.data[i].practices[0].visit_address.street + '</p>' +
+            '<p>' + doctor.data[i].practices[0].visit_address.city + "," +
+            doctor.data[i].practices[0].visit_address.state + " " +
+            doctor.data[i].practices[0].visit_address.zip +
+            '</p>' +
+            '<p>' + "Excepting new Patients: " + doctor.data[i].practices[0].accepts_new_patients + '</p>' +
+            "</div>"
+          );
+        }
+      } else {
+        $("#output").append("<p>" + "No Doctors Meet Criteria" + "</p>");
       }
     }, function (error) {
       $("#error").text(`there was an Error:${error.message}`);
       $("#error").text(`there was an Error:${error.message}`);
     });
+  });
+
+  $("#byname").click(function(){
+    
   });
 });
