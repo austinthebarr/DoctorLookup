f944f841d9292230b09b97a738e049aa56b61364
Austin Barr <austinbarr@protonmail.com>
2018-08-10 16:52:14 -0700

    added a drop down menu and now user can search docs by specialty

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -6,6 +6,51 @@ import {
 } from './../src/doctor-logic.js';
 
 $(function () {
+  let newDoctor = new Doctor();
+  let specialPromise = newDoctor.getAllSpecialties();
+
+  specialPromise.then(function (response){
+    let speciality = JSON.parse(response);
+    for(let i = 0; i < speciality.data.length; i++){
+      $("#specialty").append(`<option value = "${speciality.data[i].uid}">`+
+      `${speciality.data[i].name}`
+    +"</option>");
+    }
+  }, function(error) {
+    $("#error").html("<p>"+`there was an Error:${error.message}`+"</p>");
+  });
+  //click to search by  specialty
+  $("#bySpecialty").click(function(){
+    $("#output").empty();
+    let input = $("#specialty").val();
+    let newDoctor = new Doctor();
+    let findDoc = newDoctor.findDoctorBySpecialty(input);
+
+    findDoc.then(function (response) {
+      let doctor = JSON.parse(response);
+      if (doctor.data.length != 0) {
+        for (let i = 0; i < doctor.data.length; i++) {
+          $("#output").append("<div>" +
+            '<h2>' + doctor.data[i].profile.first_name + " " + doctor.data[i].profile.last_name + '</h2>' +
+            '<p>' + "Phone Number: " + doctor.data[i].practices[0].phones[0].number + '</p>' +
+            '<p>' + doctor.data[i].practices[0].visit_address.street + '</p>' +
+            '<p>' + doctor.data[i].practices[0].visit_address.city + "," +
+            doctor.data[i].practices[0].visit_address.state + " " +
+            doctor.data[i].practices[0].visit_address.zip +
+            '</p>' +
+            '<p>' + "Excepting new Patients: " + doctor.data[i].practices[0].accepts_new_patients + '</p>' +
+            "</div>"
+          );
+        }
+      } else {
+        $("#output").append("<p>" + "No Doctors Meet Criteria" + "</p>");
+      }
+    }, function (error) {
+      $("#error").html("<p>"+`there was an Error:${error.message}`+"</p>");
+    });
+
+  });
+  //click to search by Key Word
   $("#bykeyWord").click(function () {
     $("#output").empty();
     let input = $("#userInput").val();
@@ -38,6 +83,7 @@ $(function () {
     });
   });
 
+  //click to search by Name
   $("#byname").click(function () {
     $("#output").empty();
     let input = $("#userInput").val();
--- src/doctor-logic.js
+++ src/doctor-logic.js
@@ -30,4 +30,36 @@ export class Doctor{
       request.send();
     });
   } 
+
+  getAllSpecialties(){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/specialties?user_key=ee67c372fd6212df9b3737f3dd225883`;
+      request.onload = function(){
+        if(this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  findDoctorBySpecialty(specialty){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${specialty}&location=45.5428626%2C-122.7948136%2C100&user_location=45.5428626%2C-122.7948136&sort=distance-asc&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function(){
+        if(this.status === 200){
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
