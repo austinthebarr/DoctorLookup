18a8467e3e05dd7670ea68db255edea0e456d0f0
Austin Barr <austinbarr@protonmail.com>
2018-08-10 13:44:37 -0700

    you can now search all doctors by key word or doctor name

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -8,15 +8,15 @@ import {
 $(function () {
   $("#bykeyWord").click(function () {
     $("#output").empty();
-    const input = $("#userInput").val();
+    let input = $("#userInput").val();
     $("#userInput").val("");
 
     let newDoctor = new Doctor();
-    let keyPromise = newDoctor.findDoctorByKey(input);
+    let promise = newDoctor.findDoctorByKey(input);
 
-    keyPromise.then(function (response) {
+    promise.then(function (response) {
       let doctor = JSON.parse(response);
-      if (doctor == 0) {
+      if (doctor.data.length != 0) {
         for (let i = 0; i < doctor.data.length; i++) {
           $("#output").append("<div>" +
             '<h2>' + doctor.data[i].profile.first_name + " " + doctor.data[i].profile.last_name + '</h2>' +
@@ -39,7 +39,33 @@ $(function () {
     });
   });
 
-  $("#byname").click(function(){
-    
+  $("#byname").click(function () {
+    $("#output").empty();
+    let input = $("#userInput").val();
+    $("#userInput").val("");
+
+    let newDoctor = new Doctor();
+    let namePromise = newDoctor.findDoctorByName(input);
+
+    namePromise.then(function (response) {
+      let doctor = JSON.parse(response);
+      if(doctor.data.length != 0) {
+        for (let i = 0; i < doctor.data.length; i++) {
+          $("#output").append("<div>" +
+            '<h2>' + doctor.data[i].profile.first_name + " " + doctor.data[i].profile.last_name + '</h2>' +
+            '<p>' + "Phone Number: " + doctor.data[i].practices[0].phones[0].number + '</p>' +
+            '<p>' + doctor.data[i].practices[0].visit_address.street + '</p>' +
+            '<p>' + doctor.data[i].practices[0].visit_address.city + "," +
+            doctor.data[i].practices[0].visit_address.state + " " +
+            doctor.data[i].practices[0].visit_address.zip +
+            '</p>' +
+            '<p>' + "Excepting new Patients: " + doctor.data[i].practices[0].accepts_new_patients + '</p>' +
+            "</div>"
+          );
+        }
+      } else {
+        $("#output").append("<p>" + "No Doctors Meet Criteria" + "</p>");
+      }
+    });
   });
 });
